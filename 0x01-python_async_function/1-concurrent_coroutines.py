65a224e199cca7af164907c6c3838275f46c3dcc
mohammed ouzerzo <mohamed.ouz2016@gmail.com>
2024-05-06 16:52:40 +0000

    fix pstyle

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,10 +2,9 @@
 """
 Let's execute multiple coroutines at the same time with async
 """
-
-wait_random = __import__('0-basic_async_syntax').wait_random
 import asyncio
 from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Tasks"""
-task_wait_random = __import__('3-tasks').task_wait_random
 import asyncio
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n, max_delay):
