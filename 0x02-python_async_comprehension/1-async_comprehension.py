53bd9104a67369dd0442ca5748745974ac446753
ouzerzou <mohamed.ouz2016@gmail.com>
2024-05-07 22:31:47 +0100

    last task

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,5 +7,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """function async_comprehension"""
     rand_num = [i async for i in async_generator()]
     return rand_num
