146adbc69e24f5be7955de9b7210856dabc50e9a
ouzerzou <mohamed.ouz2016@gmail.com>
2024-05-28 01:32:39 +0100

    fix error

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python3
+"""Parameterize a unit test"""
+
 import unittest
 from unittest.mock import patch
 from parameterized import parameterized
 from client import GithubOrgClient
+
 
 class TestGithubOrgClient(unittest.TestCase):
     """class  TestGithubOrgClient"""
@@ -13,9 +17,7 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch('client.get_json')
     def test_org(self, org_name, expected_org, mock_get_json):
         mock_get_json.return_value = expected_org
-        
         client = GithubOrgClient(org_name)
         result = client.org
-        
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
         self.assertEqual(result, expected_org)
