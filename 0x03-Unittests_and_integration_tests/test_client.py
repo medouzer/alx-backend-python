399ecd6c8f6990076afe725381b8c2cc5f8a2038
ouzerzou <mohamed.ouz2016@gmail.com>
2024-05-28 01:34:42 +0100

    new things

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """Parameterize a unit test"""
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -21,3 +21,11 @@ class TestGithubOrgClient(unittest.TestCase):
         result = client.org
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
         self.assertEqual(result, expected_org)
+
+    def test_public_repos_url(self):
+        test_payload = {"repos_url": "https://api.github.com/orgs/test/repos"}
+        with patch('client.GithubOrgClient.org', new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = test_payload
+            client = GithubOrgClient("test")
+            result = client._public_repos_url
+            self.assertEqual(result, test_payload["repos_url"])
